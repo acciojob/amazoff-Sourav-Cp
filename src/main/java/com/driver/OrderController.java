add9e0cb724b6dfd13929188b332fb8b3e45cad6
Sourav <sourav10singh11@gmail.com>
2023-05-31 11:09:28 +0530

    Lombok Added

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -2,6 +2,8 @@ package com.driver;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@AllArgsConstructor
+@NoArgsConstructor
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
